88e1a134f7b16f4a11c109b42cf2130b79f5b4ad
charlie <charlieni0613@gmail.com>
2025-01-27 19:20:04 -0500

    cat idle behaviour implementation, cats move randomly in idle state

--- Assets/Scripts/Cat/CatNavigator.cs
+++ Assets/Scripts/Cat/CatNavigator.cs
@@ -4,13 +4,36 @@ using UnityEngine.AI;
 
 namespace Cat {
     public class CatNavigator : MonoBehaviour {
-        /* Randomly move around when set to true */
-        [NonSerialized] public bool RandomMove = true;
         public NavMeshAgent Agent;
-        
+        public Rigidbody Rigidbody;
+        public BoxCollider Collider;
+
+        protected void Awake() {
+            /* setup rigid body */
+            Rigidbody = GetComponent<Rigidbody>();
+            if (Rigidbody == null) Rigidbody = gameObject.AddComponent<Rigidbody>();
+            Rigidbody.useGravity = true;
+            
+            /* Make sure the box collider exists */
+            Collider = GetComponent<BoxCollider>();
+            if (Collider == null) Collider = gameObject.AddComponent<BoxCollider>();
+            
+            /* Make sure the nav mesh agent exists */
+            Agent = GetComponent<NavMeshAgent>();
+            if (Agent == null) Agent = gameObject.AddComponent<NavMeshAgent>();
+            
+            /* Update fields based on server settings */
+            Agent.speed = Server.Server.Instance.CatMoveSpeed;
+        }
+
         /* Move to a specific world position */
         public void MoveTo(Vector3 position) {
-            throw new NotImplementedException();
+            /* y field of this position has to be the height of terrain */
+            position.y = Terrain.TerrainManager.Instance.Terrain.SampleHeight(position);
+            
+            /* Move agent to given location */
+            Agent.SetDestination(position);
+            Debug.Log("Moving to " + position);
         }
     }
 }
