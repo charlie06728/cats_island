3c95ea9287472020147a6c4588d893215ed0c61d
charlie <charlieni0613@gmail.com>
2025-02-24 15:10:17 -0500

    zoom in out

--- Assets/Scripts/Player/CameraObject.cs
+++ Assets/Scripts/Player/CameraObject.cs
@@ -24,6 +24,7 @@ namespace Player {
         [NonSerialized] public Photo CurrentPhoto;
         public float zoom_max = 30f; // Max amount of zoom
         public float zoom_speed = 0.5f; // Speed at which scrolling zooms in/out
+        public float zoom_speed_controller = 1000f;
         MeshRenderer hand_renderer;
         MeshRenderer camera_renderer;
         MeshRenderer screen_renderer;
@@ -84,6 +85,14 @@ namespace Player {
             // photoCamera.fieldOfView = Camera.main.fieldOfView;
             if (_isCameraMode) {
                 zoomScroll.value += Input.GetAxis("Mouse ScrollWheel") * zoom_speed; // I
+                
+                /* Read input ZoomIn and ZoomOut */
+                if (Server.Server.Instance.InputActionMap["ZoomIn"].IsPressed()) {
+                    zoomScroll.value -= zoom_speed_controller * Time.deltaTime;
+                }
+                if (Server.Server.Instance.InputActionMap["ZoomOut"].IsPressed()) {
+                    zoomScroll.value += zoom_speed_controller * Time.deltaTime;
+                }
                 
                 // Clamp scroll value
                 if (zoomScroll.value > 1) {
