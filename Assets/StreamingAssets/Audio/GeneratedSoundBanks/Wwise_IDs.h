22714052241fec3f492d648fbaeebc1ac0e5e5ed
AnthonyGunadi <anthony.gunadi7@gmail.com>
2025-03-07 11:55:32 -0500

    fixed grass footsteps. added sand footsteps, added music soundbank and generated

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID MUSIC_FOREST = 2181728358U;
         static const AkUniqueID SFX_FOOTSTEPS = 3364658470U;
     } // namespace EVENTS
 
@@ -40,6 +41,7 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID SFX = 393239870U;
     } // namespace BANKS
 
