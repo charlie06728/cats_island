46ec1e9dac9da1589aed97b736a3c3997033a7b4
ElieMusic <elienna.wang@mail.utoronto.ca>
2025-03-09 13:04:22 -0400

    cats meows

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,9 +13,12 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID MUSIC_FOREST = 2181728358U;
-        static const AkUniqueID SFX_ALBUM = 601098146U;
+        static const AkUniqueID BENGAL_HUNGRY_MEOW = 1968854379U;
+        static const AkUniqueID GREYCAT_HUNGRY_MEOW = 1995037903U;
+        static const AkUniqueID RAGDOLL_HUNGRY_MEOW = 2846976621U;
         static const AkUniqueID SFX_FOOTSTEPS = 3364658470U;
+        static const AkUniqueID TABBY_HUNGRY_MEOW = 2553855402U;
+        static const AkUniqueID TUXEDO_HUNGRY_MEOW = 4111794441U;
     } // namespace EVENTS
 
     namespace SWITCHES
@@ -42,7 +45,6 @@ namespace AK
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
-        static const AkUniqueID MUSIC = 3991942870U;
         static const AkUniqueID SFX = 393239870U;
     } // namespace BANKS
 
